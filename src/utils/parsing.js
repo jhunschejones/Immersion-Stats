c9874b1ed9106ec4ee5287db28229208b2ca5b44
Joshua Hunsche Jones <jhunschejones@github.com>
2022-11-17 10:37:15 -0600

    Update docs and param naming for parsing function

--- src/utils/parsing.js
+++ src/utils/parsing.js
@@ -1,11 +1,17 @@
 import Papa from "papaparse";
 
-export const parseCsvFile = (file, setState, log = false) => {
+/**
+ * parses a csv file and passes the results to a callback function
+ * @param {file} file - the csv file to parse
+ * @param {function} callback - the callback to pass the parsed data to
+ * @param {boolean} log - whether to console log out the results of the file parsing
+ */
+export const parseCsvFile = (file, callback, log = false) => {
   Papa.parse(file, {
     header: true,
     skipEmptyLines: "greedy",
     complete: (results) => {
-      setState(results.data);
+      callback(results.data);
       if (log) {
         console.log(results.data);
       }
