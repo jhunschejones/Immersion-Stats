9be2c531e446b127a15f9b2a38c4d3bdb283a7d8
Joshua Hunsche Jones <jhunschejones@github.com>
2023-05-02 14:40:01 -0500

    Update jpdb fixture data

--- src/tests/components/JpdbReviews.test.js
+++ src/tests/components/JpdbReviews.test.js
@@ -28,7 +28,7 @@ describe("JpdbReviews", () => {
     );
     await screen.findByText(/jpdb.io Reviews/i);
     await screen.findByText(/2\/4\/2023, 75 minutes/i); // one of the expected heatmap cells
-    await screen.findByText(/3hrs 28mins/i); // total study time shown in time label
+    await screen.findByText(/80hrs 58mins/i); // total study time shown in time label
   });
 
   it("shows time label for a specific day when clicked", async () => {
@@ -45,17 +45,17 @@ describe("JpdbReviews", () => {
     );
 
     // default state shows total study time and days studied
-    await screen.findByText(/3hrs 28mins/i); // total study time shown in time label
-    await screen.findByText(/4 days/i); // total study days shown in day label
+    await screen.findByText(/80hrs 58mins/i); // total study time shown in time label
+    await screen.findByText(/89 days/i); // total study days shown in day label
 
     // click on a day to see the date and time studied for that day
     await user.click(container.getElementsByClassName("color-scale-3")[0]);
-    await screen.findByText(/2\/4\/2023/i, {selector: "[data-testid='day-label']"}); // day label with one days date
-    await screen.findByText(/75 minutes/i, {selector: "[data-testid='time-label']"}); // time label with one days time
+    await screen.findByText(/2\/28\/2023/i, {selector: "[data-testid='day-label']"}); // day label with one days date
+    await screen.findByText(/170 minutes/i, {selector: "[data-testid='time-label']"}); // time label with one days time
 
     // click on the body
     await user.click(container.getElementsByClassName("ReviewsHeatmap")[0]);
-    await screen.findByText(/3hrs 28mins/i); // total study time shown in time label
-    await screen.findByText(/4 days/i); // total study days shown in day label
+    await screen.findByText(/80hrs 58mins/i); // total study time shown in time label
+    await screen.findByText(/89 days/i); // total study days shown in day label
   });
 });
